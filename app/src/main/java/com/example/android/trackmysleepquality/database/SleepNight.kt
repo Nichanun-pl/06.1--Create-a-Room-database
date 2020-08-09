99348796cade79abe12e63f177748d51dcc51090
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-10 00:38:58 +0700

    Step 1: Create the SleepNight entity

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -23,7 +24,13 @@ import androidx.room.PrimaryKey
 data class SleepNight(
         @PrimaryKey(autoGenerate = true)
         var nightId: Long = 0L,
+
+        @ColumnInfo(name = "start_time_milli")
         val startTimeMilli: Long = System.currentTimeMillis(),
+
+        @ColumnInfo(name = "end_time_milli")
         var endTimeMilli: Long = startTimeMilli,
+
+        @ColumnInfo(name = "quality_rating")
         var sleepQuality: Int = -1
 )

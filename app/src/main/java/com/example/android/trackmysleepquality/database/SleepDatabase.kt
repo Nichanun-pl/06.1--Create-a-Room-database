04115753dc8cfa8d142bb9a4bad1aac8b641450b
Nichanun <nichanun.pl@rmuti.ac.th>
2020-08-10 00:49:03 +0700

    Step 1: Create the database

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -20,4 +20,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [SleepNight::class], version = 1, exportSchema = false)
-abstract class SleepDatabase : RoomDatabase() {}
+abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+    }
+}
